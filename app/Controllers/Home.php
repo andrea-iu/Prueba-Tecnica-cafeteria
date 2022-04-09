a14a7cbba3052f44b077b1273f56341078d3b809
ProyectoTecnoparque <85948237+ProyectoTecnoparque@users.noreply.github.com>
2022-04-09 18:10:14 -0500

    upload

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,12 +2,35 @@
 
 namespace App\Controllers;
 
+use App\Controllers\BaseController;
+	use App\Models\ProductosModel;
+	use App\Models\UnidadesModel;
+	use App\Models\CategoriasModel;
+
 class Home extends BaseController
 {
-    public function index()
-    {
+    protected $productos;
+    protected$reglas;
+
+
+    public function __construct(){
+        $this->productos = new ProductosModel();
+        $this->unidades = new UnidadesModel();
+        $this->categorias = new CategoriasModel();
+
+          
+    } 
+
+
+
+    public function index($estado = 1){
+
+        $productos = $this->productos->where('estado',$estado)->findAll();
+        $data =['titulo' => 'Productos' , 'datos' => $productos];
+
         echo view('header');
-        echo view('tables');
+        echo view('productos/productos', $data);
         echo view('footer');
+
     }
 }
